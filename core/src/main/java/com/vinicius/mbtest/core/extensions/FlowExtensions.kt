4ea746e5cb7df1e88d97dbc7ed6e1874082d5f08
Vinicius Silva <viniciusjns@gmail.com>
2025-07-20 10:36:36 -0300

    improve code

--- core/src/main/java/com/vinicius/mbtest/core/extensions/FlowExtensions.kt
+++ core/src/main/java/com/vinicius/mbtest/core/extensions/FlowExtensions.kt
@@ -6,7 +6,11 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 @Composable
 inline fun <reified T> Flow<T>.observeAsActions(
@@ -21,5 +25,19 @@ inline fun <reified T> Flow<T>.observeAsActions(
                 Lifecycle.State.STARTED
             ).collect { block(it) }
         }
+    }
+}
+
+fun <T> CoroutineScope.launchFlow(
+    data: () -> Flow<T>,
+    onStart: (() -> Unit)? = null,
+    onSuccess: (T) -> Unit,
+    onError: (Throwable) -> Unit
+) {
+    this.launch {
+        data()
+            .onStart { onStart?.invoke() }
+            .catch { exception -> onError(exception) }
+            .collect { result -> onSuccess(result) }
     }
 }
--- features/exchanges/impl/src/main/java/com/vinicius/mbtest/features/exchanges/impl/presentation/viewModel/ExchangesViewModel.kt
+++ features/exchanges/impl/src/main/java/com/vinicius/mbtest/features/exchanges/impl/presentation/viewModel/ExchangesViewModel.kt
@@ -1,13 +1,13 @@
 import androidx.lifecycle.viewModelScope
+import com.vinicius.mbtest.core.extensions.launchFlow
 import com.vinicius.mbtest.core.viewModel.BaseViewModel
 import com.vinicius.mbtest.core.viewModel.IViewIntent
+import com.vinicius.mbtest.features.exchanges.domain.model.Exchange
 import com.vinicius.mbtest.features.exchanges.domain.useCase.GetExchangesUseCase
 import com.vinicius.mbtest.features.exchanges.impl.presentation.action.ExchangesAction
 import com.vinicius.mbtest.features.exchanges.impl.presentation.mapper.toDataUi
 import com.vinicius.mbtest.features.exchanges.impl.presentation.state.ExchangesSyncState
 import com.vinicius.mbtest.features.exchanges.impl.presentation.state.ExchangesViewState
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 
 sealed class ExchangesViewIntent : IViewIntent {
     object FetchExchanges : ExchangesViewIntent()
@@ -27,23 +27,36 @@ class ExchangesViewModel(
     }
 
     private fun fetchExchanges() {
-        viewModelScope.launch {
-            setState { this.copy(syncState = ExchangesSyncState.Loading) }
+        if (state.value.exchanges.isNotEmpty()) return
 
-            getExchangesUseCase()
-                .catch { exception ->
-                    setState {
-                        this.copy(syncState = ExchangesSyncState.Error(message = exception.localizedMessage ?: "An error occurred"))
-                    }
-                }
-                .collect { exchangesList ->
-                    val dataUi = exchangesList.map { exchanges ->
-                        exchanges.toDataUi()
-                    }
-                    setState {
-                        this.copy(exchanges = dataUi, syncState = ExchangesSyncState.Success)
-                    }
-                }
+        viewModelScope.launchFlow(
+            data = { getExchangesUseCase() },
+            onStart = {
+                setState { copy(syncState = ExchangesSyncState.Loading) }
+            },
+            onSuccess = { exchangesList ->
+                handleSuccess(exchangesList)
+            },
+            onError = { exception ->
+                handleError(exception)
+            }
+        )
+    }
+
+    private fun handleSuccess(exchangesList: List<Exchange>) {
+        val dataUi = exchangesList.map { it.toDataUi() }
+        setState {
+            copy(exchanges = dataUi, syncState = ExchangesSyncState.Success)
+        }
+    }
+
+    private fun handleError(exception: Throwable) {
+        setState {
+            copy(
+                syncState = ExchangesSyncState.Error(
+                    message = exception.localizedMessage ?: "An error occurred"
+                )
+            )
         }
     }
 
